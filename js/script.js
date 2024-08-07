a83bbf8dadfd322364cc4b5a971d55275baed161
riosaputra17 <riobadrun1721@gmail.com>
2024-08-07 20:05:19 +0700

    Komit Awal

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// kertika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar navbar untuk menghilangkan sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
